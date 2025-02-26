897be9ef5eb315c10da1e772c74694af953a107a
tuezox <mr9100567@gmail.com>
2025-02-25 21:26:58 -0300

    lib_atualizado

---
+++ lib/tarefas_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class TarefasPage extends StatefulWidget {
+  const TarefasPage({super.key});
+
+  @override
+  State<TarefasPage> createState() => _TarefasPageState();
+}
+
+class _TarefasPageState extends State<TarefasPage> {
+ List<String> tarefas = [
+ "Pagar contas",
+ "Comprar roupas"
+];
+
+String novaTarefa = "";
+
+void addTarefa(){
+  if(novaTarefa != ""){
+  setState(() {
+   tarefas.add(novaTarefa); 
+  });
+  }
+}
+void removeTarefas(String tarefa){
+  setState(() {
+    tarefas.remove(tarefa);
+  });
+  
+}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("tarefas page"),
+        backgroundColor: Colors.cyan,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [
+            ...tarefas.map(
+              (tarefa)=> GestureDetector(
+                onTap: (){
+                  removeTarefas(tarefa);
+                },
+                child: Card(
+                            child:Container(
+                padding: EdgeInsets.all(10),
+                child: Text(tarefa))),
+              )),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 60,
+        color: Colors.cyan,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+              width: 400,
+              child: TextFormField(
+                onChanged: (valor){
+                  novaTarefa = valor;
+                }, 
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(), 
+                  )
+                ),
+              ),
+              GestureDetector(
+                onTap: (){
+                addTarefa();
+
+                },
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.white,
+                  child: Icon(Icons.add_shopping_cart),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
