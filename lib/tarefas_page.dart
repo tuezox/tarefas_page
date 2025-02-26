3c4eaa77651250eaa5076bcb0a6f62603e395e92
tuezox <mr9100567@gmail.com>
2025-02-26 19:54:48 -0300

    files atual

--- lib/tarefas_page.dart
+++ lib/tarefas_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class TarefasPage extends StatefulWidget {
   const TarefasPage({super.key});
@@ -8,6 +9,7 @@ class TarefasPage extends StatefulWidget {
 }
 
 class _TarefasPageState extends State<TarefasPage> {
+  late SharedPreferences prefs;
  List<String> tarefas = [
  "Pagar contas",
  "Comprar roupas"
@@ -20,14 +22,29 @@ void addTarefa(){
   setState(() {
    tarefas.add(novaTarefa); 
   });
+  prefs.setStringList("tarefas", tarefas);
   }
 }
 void removeTarefas(String tarefa){
   setState(() {
     tarefas.remove(tarefa);
+     prefs.setStringList("tarefas", tarefas);
   });
   
 }
+
+@override
+  void initState() {
+    super.initState();
+    carregarTarefas();
+  }
+
+  Future<void> carregarTarefas() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {
+       tarefas = prefs.getStringList("tarefas")??[];
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
